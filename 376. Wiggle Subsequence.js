3403b2edd4a837bd8fdbcb4c9644b9de99a1c09d
leonardo <leo.carvalholkp@gmail.com>
2022-07-03 11:03:35 -0300

    Wiggle Subsequence

---
+++ 376. Wiggle Subsequence.js
@@ -0,0 +1,10 @@
+
+const wiggleMaxLength = function(nums) {
+  let inc = 1
+  let dec = 1
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] > nums[i-1]) inc = dec + 1
+    else if(nums[i] < nums[i - 1]) dec = inc + 1
+  }
+  return Math.max(inc, dec)
+}
