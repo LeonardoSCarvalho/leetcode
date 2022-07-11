dd5858f6068d931de8e9d2d3f7d3a1d915c4cf3b
leonardo <leo.carvalholkp@gmail.com>
2022-07-11 13:06:00 -0300

    3. Longest Substring Without Repeating Characters

---
+++ 3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,16 @@
+const lengthOfLongestSubstring = function (s) {
+  const map = new Map()
+  let left = 0
+  let right = 0
+  let maxSize = 0
+  while (right < s.length) {
+    if (!map.has(s[right])) {
+      map.set(s[right], 1)
+      right++
+      maxSize = Math.max(map.size, maxSize)
+    } else {
+      map.delete(s[left++])
+    }
+  }
+  return maxSize
+};
