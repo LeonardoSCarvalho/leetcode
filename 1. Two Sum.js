0fc3cbf48e087e6181cfa52a1cf5df0d868051ca
leonardo <leo.carvalholkp@gmail.com>
2022-08-02 21:02:17 -0300

    1. Two Sum

---
+++ 1. Two Sum.js
@@ -0,0 +1,10 @@
+const twoSum = function(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+    map[nums[i]] = i;
+  }
+}
