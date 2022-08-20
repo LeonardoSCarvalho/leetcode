cfdd500187396f8365d31e77ed3a48ba4cb349fb
leonardo <leo.carvalholkp@gmail.com>
2022-08-19 21:17:39 -0300

    12. Integer to Roman

---
+++ 12. Integer to Roman.js
@@ -0,0 +1,26 @@
+const intToRoman = function(num) {
+    let roman = '';
+    const romanNum = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+    for (var i = 0; i < romanNum.length; i++) {
+        while (num >= romanNum[i][1]) {
+            roman += romanNum[i][0];
+            num -= romanNum[i][1];
+        }
+    }
+    return roman;
+    
+};
